f9f8b7a0b0cb187fd77e800fcf396880998fb3fd
judewh00 <105837575+judewh00@users.noreply.github.com>
2022-05-20 18:09:21 -0400

    Update fbimindmap.mm

--- e-library website/mindmap/fbimindmap.mm
+++ e-library website/mindmap/fbimindmap.mm
@@ -2,9 +2,10 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1651178639912" ID="ID_1998236401" MODIFIED="1651249127247" TEXT="Origins of the FBI">
 <node CREATED="1651178777998" ID="ID_1606924966" LINK="lincolnassassination.html" MODIFIED="1653022476083" POSITION="right" TEXT="Lincoln assassination angle"/>
-<node CREATED="1651249173852" ID="ID_862472830" LINK="mckinleyassassination.html" MODIFIED="1653022789333" POSITION="left" TEXT="Leon Czolgosz/McKinley assassination">
+<node CREATED="1651249173852" ID="ID_862472830" LINK="mckinleyassassination.html" MODIFIED="1653022831997" POSITION="left" TEXT="Leon Czolgosz/McKinley assassination">
 <arrowlink DESTINATION="ID_862472830" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1739000730" STARTARROW="None" STARTINCLINATION="0;0;"/>
 <linktarget COLOR="#b0b0b0" DESTINATION="ID_862472830" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1739000730" SOURCE="ID_862472830" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font NAME="SansSerif" SIZE="12"/>
 </node>
 <node CREATED="1653012508075" ID="ID_1737397744" LINK="moderncointelpro.html" MODIFIED="1653022491053" POSITION="right" TEXT="Modern-day COINTELPRO"/>
 </node>
